47fc9f1381a0e3902cf8238393fafef2d5721990
darshpatel123 <81002213+darshpatel123@users.noreply.github.com>
2021-04-22 14:49:05 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 function one(){
- window.alert("The ip is the ip of the server for minecraft open it then click on multiplayer then click on add server or direct server and type in server slot 192.168.1.53:25565 than done")   
+ window.alert("The ip is the ip of the server for minecraft open it then click on multiplayer then click on add server or direct server and type in server slot damdakshdamserver.ddns.net:25565 than done")   
 }
 var x = document.getElementById("Ding-sound-effect.mp3");
 
