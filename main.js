44c1912f733f33e286a6fe3c6cb062f1b3c7489d
darshpatel123 <81002213+darshpatel123@users.noreply.github.com>
2021-04-22 14:19:21 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+function one(){
+ window.alert("The ip is the ip of the server for minecraft open it then click on multiplayer then click on add server or direct server and type in server slot 192.168.1.53:25565 than done")   
+}
+var x = document.getElementById("Ding-sound-effect.mp3");
+
+function playAudio() {
+  x.play();
+}
